54af44d74a659b3589fae58623eacd504a77b668
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-02 17:07:01 +0300

    handling events with EventEmitter

--- BenFranklin.js
+++ BenFranklin.js
@@ -1,9 +1,25 @@
-const events = require('events')
-const emitter = new events.EventEmitter()
+const EventEmitter = require('events').EventEmitter
+const util = require('util')
 
-emitter.on('customEvent', (message, status) => {
-  console.log(`${status}: ${message}`)
+const Person = function(name) {
+  this.name = name
+}
+util.inherits(Person, EventEmitter)
+
+const ben = new Person('Ben Franklin')
+
+ben.on('speak', function(said) {
+  console.log(`${this.name}: ${said}`)
 })
 
-emitter.emit('customEvent', 'Hello World', 200)
+ben.emit('speak', 'You may delay, but time will not.')
+
+// const events = require('events')
+// const emitter = new events.EventEmitter()
+
+// emitter.on('customEvent', (message, status) => {
+//   console.log(`${status}: ${message}`)
+// })
+
+// emitter.emit('customEvent', 'Hello World', 200)
 
