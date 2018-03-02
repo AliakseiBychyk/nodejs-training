1f963d3c9c08521521102fb7da0836d41eca8671
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-02 13:50:02 +0300

    process.stdin and stdout

--- process.stdin.js
+++ process.stdin.js
@@ -10,12 +10,24 @@ let questions = [
 let answers = []
 
 const ask = i => {
-  process.stdout.write(`\n\n\n ${questions[i]}`)
+  process.stdout.write(`\n\ ${questions[i]}`)
   process.stdout.write(' > ')
 }
 
 process.stdin.on('data', data => {
-  process.stdout.write('\n' + data.toString().trim() + '\n')
+  // process.stdout.write('\n' + data.toString().trim() + '\n')
+  answers.push(data.toString().trim())
+  if (answers.length < questions.length) {
+    ask(answers.length)
+  } else {
+    process.exit()
+  }
+})
+
+process.on('exit', () => {
+  process.stdout.write('\n\n\n')
+  process.stdout.write(`Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later.`)
+  process.stdout.write('\n\n\n')
 })
 
 ask(0)
