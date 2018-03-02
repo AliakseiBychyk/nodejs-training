399433f5749d17754c45b829570c13f5af522693
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-02 15:26:07 +0300

    timers percent  waited

--- timers.js
+++ timers.js
@@ -1,15 +1,28 @@
 const waitTime = 3000
 let currentTime = 0
-const waitInterval = 500
+const waitInterval = 10
+let percentWaited = 0
 
 console.log('Wait for it')
 
+function writeWaitingPercent(p) {
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  process.stdout.write(`waiting... ${p} %`)
+}
+
 const interval = setInterval(() => {
   currentTime += waitInterval
-  console.log(`waiting ${currentTime/1000} seconds ...`)
+  percentWaited = Math.floor((currentTime / waitTime) * 100)
+  // console.log(`waiting ${currentTime/1000} seconds ...`)
+  writeWaitingPercent(percentWaited)
 }, waitInterval)
 
 setTimeout(() => {
   clearInterval(interval)
+  writeWaitingPercent(100)
   console.log('done')
 }, waitTime)
+
+process.stdout.write('\n\n')
+writeWaitingPercent(percentWaited)
