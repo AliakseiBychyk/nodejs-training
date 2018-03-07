45caa274108316a4a23054de0e5809cdc8f75adb
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-07 16:54:47 +0300

    express routing and CORS

--- 8-web-servers/app.js
+++ 8-web-servers/app.js
@@ -1,6 +1,21 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
+
+const skierTerms = [
+  {
+      term: "Rip",
+      defined: "To move at a high rate of speed"
+  },
+  {
+      term: "Huck",
+      defined: "To throw your body off of something, usually a natural feature like a cliff"
+  },
+  {
+      term: "Chowder",
+      defined: "Powder after it has been sufficiently skied"
+  }
+]
 
 app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`)
@@ -8,6 +23,12 @@ app.use((req, res, next) => {
 })
 
 app.use(express.static('./public'))
+
+app.use(cors())
+
+app.get('/dictionary-api', (req, res) => {
+  res.json(skierTerms)
+})
 
 
 app.listen(3000)
