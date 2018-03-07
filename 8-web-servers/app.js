fb367be0260d121fe94804ecbc5a6135c2955dc0
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-07 17:22:31 +0300

    express post bodies and params

--- 8-web-servers/app.js
+++ 8-web-servers/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 
-const skierTerms = [
+let skierTerms = [
   {
       term: "Rip",
       defined: "To move at a high rate of speed"
@@ -17,8 +18,11 @@ const skierTerms = [
   }
 ]
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use((req, res, next) => {
-  console.log(`${req.method} request for ${req.url}`)
+  console.log(`${req.method} request for ${req.url} - ${JSON.stringify(req.body)}`)
   next()
 })
 
@@ -30,6 +34,17 @@ app.get('/dictionary-api', (req, res) => {
   res.json(skierTerms)
 })
 
+app.post('/dictionary-api', (req, res) => {
+  skierTerms.push(req.body)
+  res.json(skierTerms)
+})
+
+app.delete('/dictionary-api/:term', (req, res) => {
+  skierTerms = skierTerms.filter(definition =>
+    definition.term.toLowerCase() !== req.params.term.toLowerCase()
+  )
+  res.json(skierTerms)
+})
 
 app.listen(3000)
 console.log('Express app running on port 3000')
