9de04d6b4c5b371e602c75ef115d17e99ba7e824
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-07 18:08:21 +0300

    broadcasting messages with WebSockets

--- 9-web-sockets/ws.js
+++ 9-web-sockets/ws.js
@@ -3,5 +3,14 @@ const { Server: WebSocketServer } = require('ws')
 const wss = new WebSocketServer({ port: 3000 })
 
 wss.on('connection', (ws) => {
+  ws.on('message', message => {
+    if (message === 'exit') {
+      ws.close()
+    } else {
+      wss.clients.forEach(client => {
+        client.send(message)
+      })
+    }
+  })
   ws.send('Welcome to cyber chat')
 })
