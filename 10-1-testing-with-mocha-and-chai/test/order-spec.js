913d310abd1ad8ea0e124981e04b486ddcfb0b2d
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-12 15:30:26 +0300

    advanced testing Sinon spies

--- 10-1-testing-with-mocha-and-chai/test/order-spec.js
+++ 10-1-testing-with-mocha-and-chai/test/order-spec.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai')
 const rewire = require('rewire')
 const order = rewire('../lib/order') // instead of require we rewire
+const sinon = require('sinon')
 
 describe('Ordering Items', () => {
 
@@ -10,11 +11,19 @@ describe('Ordering Items', () => {
       { sku: 'BBB', qty: 0 },
       { sku: 'CCC', qty: 3 }
     ]
+
+    this.console = {
+      log: sinon.spy()
+    }
+
     order.__set__('inventoryData', this.testData)
+    order.__set__('console', this.console)
   })
 
-  it('order an item there are enough in stock', (done) => {
-    order.orderItem('CCC', 3, () => {
+  it('order an item there are enough in stock', function(done) {
+    // arrow function won't access to the Mocha this context
+    order.orderItem('CCC', 3, () => {    
+      expect(this.console.log.callCount).to.equal(2)
       done()
     })
   })
