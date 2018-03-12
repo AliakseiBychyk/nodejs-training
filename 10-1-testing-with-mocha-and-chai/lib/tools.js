dced8c6d0d013ec7917d15ed985419969326e02c
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-12 14:34:28 +0300

    asynchronous mocha testing

--- 10-1-testing-with-mocha-and-chai/lib/tools.js
+++ 10-1-testing-with-mocha-and-chai/lib/tools.js
@@ -1,5 +1,25 @@
+const https = require('https')
+
 module.exports = {
-  printName({first, last}) {
+  
+  printName({ first, last }) {
     return `${last}, ${first}`
+  },
+
+  loadWiki({ first, last }, callback) {
+    const url = `https://en.wikipedia.org/wiki/${first}_${last}`
+    https.get(url, (res) => {
+      let body = ''
+
+      res.setEncoding('UTF-8')
+      
+      res.on('data', chunk => {
+        body += chunk
+      })
+      
+      res.on('end', () => {
+        callback(body)
+      })
+    })
   }
 }
--- 10-1-testing-with-mocha-and-chai/test/tools-spec.js
+++ 10-1-testing-with-mocha-and-chai/test/tools-spec.js
@@ -1,9 +1,28 @@
 const { expect } = require('chai')
 const tools = require('../lib/tools')
 
-describe('printName()', () => {
-  it('should print the last name first', () => {
-    const results = tools.printName({ first: 'Aleks', last: 'Byczyk' })
-    expect(results).to.equal('Byczyk, Aleks')
+describe('Tools', () => {
+  
+  describe('printName()', () => {
+  
+    it('should print the last name first', () => {
+      const results = tools.printName({ first: 'Aleks', last: 'Byczyk' })
+      expect(results).to.equal('Byczyk, Aleks')
+    })
   })
+
+  describe('loadWiki()', function () {
+    
+    // arrow function won't access the Mocha context 'this'
+    this.timeout(5000)
+    
+    it('Load Abraham Lincoln wikipedia page', (done) => {
+      tools.loadWiki({ first: 'Abraham', last: 'Lincoln' }, html => {
+        expect(html).to.be.ok
+        done()
+      })
+    })
+  })
+
 })
+
