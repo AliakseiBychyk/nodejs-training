78c529c4ce54d1b31fb3c9192be60463d0dc6048
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-12 15:54:30 +0300

    advanced testing sinon stubs

--- 10-1-testing-with-mocha-and-chai/test/order-spec.js
+++ 10-1-testing-with-mocha-and-chai/test/order-spec.js
@@ -16,8 +16,13 @@ describe('Ordering Items', () => {
       log: sinon.spy()
     }
 
+    this.warehouse = {
+      packageAndShip: sinon.stub().yields(10987654321)
+    }
+
     order.__set__('inventoryData', this.testData)
     order.__set__('console', this.console)
+    order.__set__('warehouse', this.warehouse)
   })
 
   it('order an item there are enough in stock', function(done) {
@@ -25,6 +30,22 @@ describe('Ordering Items', () => {
     order.orderItem('CCC', 3, () => {    
       expect(this.console.log.callCount).to.equal(2)
       done()
+    })
+  })
+
+  describe('Warehouse interaction', function () {
+
+    beforeEach(function() {
+      this.callback = sinon.spy()
+      order.orderItem('CCC', 2, this.callback)
+    })
+
+    it('receives a tracking number', function () {
+      expect(this.callback.calledWith(10987654321)).to.equal(true)
+    })
+    
+    it('calls packageAndShip with the correct sku and quantity', function () {
+      expect(this.warehouse.packageAndShip.calledWith('CCC', 2)).to.equal(true)
     })
   })
 })
